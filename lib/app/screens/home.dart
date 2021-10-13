eb1a99e3eaa994db5cebfa2a7c0cb987aacd1bfa
Tomoaki Sakurai <ssaku.power@gmail.com>
2021-10-13 23:08:03 +0900

    sliver

--- lib/app/screens/home.dart
+++ lib/app/screens/home.dart
@@ -4,7 +4,19 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home')),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          title: Text("Sliver appbar"),
+          centerTitle: true,
+          expandedHeight: 200,
+          backgroundColor: Colors.blueAccent,
+        ),
+        SliverFillRemaining(
+          child: Center(
+            child: Text("Hello"),
+          ),
+        )
+      ]),
     );
   }
 }
